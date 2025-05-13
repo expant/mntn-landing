04506d347d7b5561a9258f5e55befa91f408766b
Expant <anton532202@gmail.com>
2025-05-13 16:49:46 +0300

    feat: add mobile menu and animate mobile menu, animate photos with scrollTrigger

--- animations/index.js
+++ animations/index.js
@@ -1,13 +1,112 @@
 gsap.registerPlugin(SplitText);
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   ScrollSmoother.create({
-//     wrapper: "#smooth-wrapper",
-//     smooth: 1,
-//     effects: true,
-//   });
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  const mobileMenuOpenBtn = document.querySelector(".header__mobile-menu");
+  const mobileMenuExitBtn = document.querySelector(".mobile-menu__exit");
+
+  gsap.set(".mobile-menu-wrapper", { x: "100vw", duration: 0.5 });
+
+  mobileMenuOpenBtn.addEventListener("click", () => {
+    document.body.classList.add("no-scroll");
+
+    let tl = gsap.timeline({
+      onComplete: () => {
+        tl.kill();
+      },
+    });
+
+    tl.timeScale(1.5);
+    tl.fromTo(
+      ".mobile-menu-wrapper",
+      {
+        display: "none",
+        opacity: 0,
+        x: "100vw",
+      },
+      {
+        display: "block",
+        opacity: 1,
+        x: 0,
+        duration: 0.5,
+        ease: "power2.out",
+      }
+    );
+    tl.from(".mobile-menu", {
+      opacity: 0,
+      x: 200,
+    });
+  });
+
+  mobileMenuExitBtn.addEventListener("click", () => {
+    document.body.classList.remove("no-scroll");
+
+    gsap.to(".mobile-menu-wrapper", {
+      opacity: 0,
+      x: "100vw",
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  });
+
+  gsap.utils.toArray(".content__item-img").forEach((el) => {
+    gsap.from(el, {
+      x: 100,
+      opacity: 0,
+      duration: 1,
+      scrollTrigger: {
+        trigger: el,
+        start: "top center",
+        markers: true,
+      },
+    });
+  });
+
+  const hoverElements = gsap.utils.toArray(".mobile-menu__item-link");
+  hoverElements.forEach((el) => {
+    const original = el.querySelector(".original");
+    const reveal = el.querySelector(".reveal");
+
+    // gsap.set(reveal, { y: "100%", opacity: 0 });
+    el.addEventListener("mouseenter", () => {
+      gsap
+        .timeline()
+        .to(original, {
+          y: "-100%",
+          duration: 0.3,
+          ease: "power2.out",
+        })
+        .to(
+          reveal,
+          {
+            y: "0%",
+            duration: 0.3,
+            ease: "power2.out",
+          },
+          0
+        );
+    });
+
+    el.addEventListener("mouseleave", () => {
+      gsap
+        .timeline()
+        .to(reveal, {
+          y: "100%",
+          duration: 0.3,
+          ease: "power2.out",
+        })
+        .to(
+          original,
+          {
+            y: "0%",
+            duration: 0.3,
+            ease: "power2.out",
+          },
+          0
+        );
+    });
+  });
+});
 
 document.fonts.ready.then(() => {
   gsap.set(".hero__title-xl", { opacity: 1 });
@@ -27,6 +126,6 @@ document.fonts.ready.then(() => {
         ease: "expo.out",
       });
       return split;
-    }
+    },
   });
 });
